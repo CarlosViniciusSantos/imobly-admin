a43e4ba565d1ac33f98c8f2971f634af4a3aa589
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-11-15 20:49:35 -0300

    CRUD empresas completo

--- src/app/login.js
+++ src/app/login.js
@@ -30,7 +30,7 @@ export default function Login() {
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
-                login({ accessToken: data.token, ...data.company });
+                login({ accessToken: data.token, ...data.usuario });
                 await storeObjectData('userLogged', { accessToken: data.token, ...data.company });
                 router.replace('/home');
             } else {
